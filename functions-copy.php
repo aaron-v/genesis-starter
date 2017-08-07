8d91c575c19907d1dcb661654e08bd437c64c72f
aaron-v <sleep.is.optional@gmail.com>
2017-08-07 18:45:27 -0400

    work this time

---
+++ functions-copy.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Description
+ *
+ * @package		LeverageWP\Starter
+ * @since 		1.0.0
+ * @author 		Aaron Anderson
+ * @link 		http://leveragewp.com
+ * @license		GNU General Public License 2.0+
+ */
+namespace LeverageWP\Starter;
+
+include_once( 'lib/init.php' );
+
+include_once( 'lib/functions/autoload.php' );
+
+//* Start genesis
+include_once( get_template_directory() . '/lib/init.php' );
+
+// changed
